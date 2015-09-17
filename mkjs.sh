3a6d978aa9af1f12af13b815f2f3fcbc80b62fbd
Wayne O. Cochran <wcochran@vancouver.wsu.edu>
2015-09-17 09:37:27 -0700

    converted JSON -> JS

--- mkjs.sh
+++ mkjs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #
-# Uses c2json.pl script to convert glyphs from C source to JSON format
+# Uses c2js.pl script to convert glyphs from C source to JS format
 #
 
 for C in 0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z; do
-    F=glyph_$C.json
+    F=glyph_$C.js
     echo "creating $F..."
-    ./c2json.pl $C > $F
+    ./c2js.pl $C > $F
 done
